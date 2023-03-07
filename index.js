f4359411c177f8a5af3fb36c070d13892aa89a46
javier <javierkebin@gmail.com>
2023-03-07 12:53:39 -0500

    aplicando DOM para añadir elementos en html

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-const btn = document.querySelector("[data-form-btn]");
-btn.addEventListener("click", (evento) => {
-    evento.preventDefault
-    const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
-})
+const btn = document.querySelector('[data-form-btn]');
+const btnClear = document.querySelector("[data-form-clear]");
+
+const createTask = (evento) => {
+  evento.preventDefault();
+  const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  input.value = ''
+  const lista = document.querySelector('[data-form-lista]');
+  const elemento = document.createElement('li');
+  elemento.classList.add('card');
+  const contenido = `
+    <div>
+      <i class="far fa-check-square icon"></i>
+      <span class="task">${value}</span>
+    </div>
+    <i class="fas fa-trash-alt trashIcon icon" data-form-clear></i>
+  `
+  lista.appendChild(elemento);
+  elemento.innerHTML = contenido;
+}
+
+btn.addEventListener("click", createTask)
+btnClear.addEventListener("click", clear)
