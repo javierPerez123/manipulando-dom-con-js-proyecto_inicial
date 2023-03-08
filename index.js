bd4cd46c86443f401cfb5ec344f85015c4a1d874
javier <javierkebin@gmail.com>
2023-03-08 00:20:57 -0500

    aprendiendo toggle para añadir o elimianr class

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
-const btn = document.querySelector('[data-form-btn]');
-const btnClear = document.querySelector("[data-form-clear]");
-const btnCheck = document.querySelector('.far .fa-square .icon')
+(() => {
 
-const checkBox = () => {
-  const iconCheck = document.createElement('i');
-  iconCheck.classList.add('far');
-  iconCheck.classList.add('fa-square');
-  iconCheck.classList.add('icon');
-  iconCheck.dataset()
-  return iconCheck;
-}
+  const btn = document.querySelector('[data-form-btn]');
+  const btnCheck = document.querySelector('.far .fa-square .icon')
 
-const nameCourse = (text) => {
-  const course = document.createElement('span');
-  course.classList.add('task');
-  course.textContent = text
-  return course;
-}
+  const checkBox = () => {
+    const iconCheck = document.createElement('i');
+    iconCheck.classList.add('far', 'fa-square', 'icon');
+    iconCheck.addEventListener('click', activateDesactivate)
+    return iconCheck;
+  }
 
-const checkDelete = () => {
-  const iconDelete = document.createElement('i');
-  iconDelete.classList.add('fas');
-  iconDelete.classList.add('fa-trash-alt');
-  iconDelete.classList.add('trashIcon');
-  iconDelete.classList.add('icon');
-  return iconDelete;
-}
+  const activateDesactivate = (evento) => {
+    const elemento = evento.target;
+    elemento.classList.toggle('fas');
+    elemento.classList.toggle('fa-check-square');
+    elemento.classList.toggle('completeIcon');
+    elemento.classList.toggle('far');
+    elemento.classList.toggle('fa-square');
+  }
 
-const createTask = (evento) => {
-  evento.preventDefault();
-  const input = document.querySelector('[data-form-input]');
-  const value = input.value;
-  input.value = '';
-  const item = document.createElement('li');
-  item.classList.add('card');
-  const lista = document.querySelector('[data-form-lista]');
-  const contenedor = document.createElement('div');
-  contenedor.appendChild(checkBox());
-  contenedor.appendChild(nameCourse(value));
-  item.appendChild(contenedor)
-  item.appendChild(checkDelete());
-  lista.appendChild(item);
-}
+  const nameCourse = (text) => {
+    const course = document.createElement('span');
+    course.classList.add('task');
+    course.textContent = text
+    return course;
+  }
 
+  const checkDelete = () => {
+    const iconDelete = document.createElement('i');
+    iconDelete.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
+    iconDelete.addEventListener('click', checkDeleteActivate)
+    return iconDelete;
+  }
 
-btn.addEventListener("click", createTask)
-btnCheck.addEventListener("click", createTask)
+  const checkDeleteActivate = (evento) => {
+    evento.preventDefault();
+    const lista = document.querySelector('[data-form-lista]');
+    const item = document.querySelector('li')
+    lista.removeChild(item)
+  }
+
+  const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    input.value = '';
+    const item = document.createElement('li');
+    item.classList.add('card');
+    const lista = document.querySelector('[data-form-lista]');
+    const contenedor = document.createElement('div');
+    contenedor.appendChild(checkBox());
+    contenedor.appendChild(nameCourse(value));
+    item.appendChild(contenedor)
+    item.appendChild(checkDelete());
+    lista.appendChild(item);
+
+  }
+
+  btn.addEventListener("click", createTask)
+
+})
+
+  ();
