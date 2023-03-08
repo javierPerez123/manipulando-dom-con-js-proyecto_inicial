6266168fae7dc5a8ff520abba1d0bb4713afdde6
javier <javierkebin@gmail.com>
2023-03-07 22:40:29 -0500

    agregar lineas de html desde js

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 const btn = document.querySelector('[data-form-btn]');
 const btnClear = document.querySelector("[data-form-clear]");
+const btnCheck = document.querySelector('.far .fa-square .icon')
+
+const checkBox = () => {
+  const iconCheck = document.createElement('i');
+  iconCheck.classList.add('far');
+  iconCheck.classList.add('fa-square');
+  iconCheck.classList.add('icon');
+  iconCheck.dataset()
+  return iconCheck;
+}
+
+const nameCourse = (text) => {
+  const course = document.createElement('span');
+  course.classList.add('task');
+  course.textContent = text
+  return course;
+}
+
+const checkDelete = () => {
+  const iconDelete = document.createElement('i');
+  iconDelete.classList.add('fas');
+  iconDelete.classList.add('fa-trash-alt');
+  iconDelete.classList.add('trashIcon');
+  iconDelete.classList.add('icon');
+  return iconDelete;
+}
 
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector('[data-form-input]');
   const value = input.value;
-  input.value = ''
+  input.value = '';
+  const item = document.createElement('li');
+  item.classList.add('card');
   const lista = document.querySelector('[data-form-lista]');
-  const elemento = document.createElement('li');
-  elemento.classList.add('card');
-  const contenido = `
-    <div>
-      <i class="far fa-check-square icon"></i>
-      <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon" data-form-clear></i>
-  `
-  lista.appendChild(elemento);
-  elemento.innerHTML = contenido;
+  const contenedor = document.createElement('div');
+  contenedor.appendChild(checkBox());
+  contenedor.appendChild(nameCourse(value));
+  item.appendChild(contenedor)
+  item.appendChild(checkDelete());
+  lista.appendChild(item);
 }
 
+
 btn.addEventListener("click", createTask)
-btnClear.addEventListener("click", clear)
+btnCheck.addEventListener("click", createTask)
